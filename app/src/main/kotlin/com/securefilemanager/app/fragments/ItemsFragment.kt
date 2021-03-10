40fc8e05f422220c44d30931a8b547968113701d
Alfavio <michal@olencin.com>
2021-03-10 14:30:02 +0100

    Animate recycle view

--- app/src/main/kotlin/com/securefilemanager/app/fragments/ItemsFragment.kt
+++ app/src/main/kotlin/com/securefilemanager/app/fragments/ItemsFragment.kt
@@ -213,6 +213,7 @@ class ItemsFragment : Fragment(), ItemOperationsListener, Breadcrumbs.Breadcrumb
                     items_list.adapter = this
                 }
 
+                items_list.scheduleLayoutAnimation()
                 items_fastscroller.setViews(items_list, mView.items_swipe_refresh) {
                     val listItem = getRecyclerAdapter()?.listItems?.getOrNull(it)
                     items_fastscroller.updateBubbleText(
